26703291a2984845b2928131f01d1401c8b89ee0
K-Kyler <khaiquang690@gmail.com>
2021-03-16 13:17:24 +0700

    done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -65,9 +65,6 @@ module.exports.postSignIn = (req, res) => {
         });
 
         // Send request to Front-end to redirect to weather page
-        res.json({
-            code: 1,
-            message: "Sign in successful with Facebook",
-        });
+        res.redirect("/");
     }
 };
